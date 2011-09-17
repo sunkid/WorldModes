70d29a5032985411adfa9bc4515e5b46d826d607
sunkid <sunkid@iminurnetz.com>
2011-09-16 20:14:30 -0700

    version 1.0

---
+++ plugin/src/main/java/com/iminurnetz/bukkit/plugin/worldmodes/WMPlayerListener.java
@@ -0,0 +1,88 @@
+/**
+ * LICENSING
+ * 
+ * This software is copyright by sunkid <sunkid@iminurnetz.com> and is
+ * distributed under a dual license:
+ * 
+ * Non-Commercial Use:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Commercial Use:
+ *    Please contact sunkid@iminurnetz.com
+ */
+package com.iminurnetz.bukkit.plugin.worldmodes;
+
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+import com.iminurnetz.bukkit.permissions.PermissionHandler;
+import com.iminurnetz.bukkit.permissions.PermissionHandlerService;
+import com.iminurnetz.bukkit.plugin.util.MessageUtils;
+
+public class WMPlayerListener extends PlayerListener implements Listener {
+    private static final String PERMISSION_PREFIX = "worldmodes.";
+    private final WorldModesPlugin plugin;
+    private final PermissionHandler permissionHandler;
+
+    public WMPlayerListener(WorldModesPlugin plugin) {
+        this.permissionHandler = PermissionHandlerService.getHandler(plugin);
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
+        Player player = event.getPlayer();
+        if (!hasPermission(player, event.getNewGameMode())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @Override
+    public void onPlayerMove(PlayerMoveEvent event) {
+        checkWorldTransition(event);
+    }
+
+    @Override
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        checkWorldTransition(event);
+    }
+
+    private void checkWorldTransition(final PlayerMoveEvent event) {
+        if (event.isCancelled() || event.getFrom().getWorld().equals(event.getTo().getWorld())) {
+            return;
+        }
+
+        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+            public void run() {
+                Player player = event.getPlayer();
+                GameMode mode = player.getGameMode();
+                if (!hasPermission(player, mode)) {
+                    GameMode newMode = GameMode.getByValue(mode.getValue() == 0 ? 1 : 0);
+                    player.setGameMode(newMode);
+                }
+            }
+        }, 1);
+    }
+
+    private boolean hasPermission(Player player, GameMode mode) {
+        String permission = PERMISSION_PREFIX + mode.name().toLowerCase();
+        return permissionHandler.hasPermission(player, permission);
+    }
+}
---
+++ plugin/src/main/java/com/iminurnetz/bukkit/plugin/worldmodes/WorldModesPlugin.java
@@ -0,0 +1,41 @@
+/**
+ * LICENSING
+ * 
+ * This software is copyright by sunkid <sunkid@iminurnetz.com> and is
+ * distributed under a dual license:
+ * 
+ * Non-Commercial Use:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Commercial Use:
+ *    Please contact sunkid@iminurnetz.com
+ */
+package com.iminurnetz.bukkit.plugin.worldmodes;
+
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginManager;
+
+import com.iminurnetz.bukkit.plugin.BukkitPlugin;
+
+public class WorldModesPlugin extends BukkitPlugin {
+    @Override
+    public void enablePlugin() throws Exception {
+        PluginManager pm = getServer().getPluginManager();
+        WMPlayerListener listener = new WMPlayerListener(this);
+        pm.registerEvent(Type.PLAYER_GAME_MODE_CHANGE, listener, Priority.Lowest, this);
+        pm.registerEvent(Type.PLAYER_MOVE, listener, Priority.Monitor, this);
+        pm.registerEvent(Type.PLAYER_TELEPORT, listener, Priority.Monitor, this);
+    }
+}
